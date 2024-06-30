8112307cc77851fb731c16c8b58277de3c951049
Pablo <pabloalvaradovazquez10@gmail.com>
2024-06-30 13:03:19 -0600

    image

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -5,19 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage; // Add this import statement
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 class ImagenController extends Controller
 {
     public function upload(Request $request)
     {
         // Validar la solicitud
 
-        
-        $request->validate([
-            'image' => 'required|image|max:2048', // Puedes ajustar las reglas de validación según tus necesidades
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        dd($request->image);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+
+        // Subir la imagen al disco 'public'
+        
+
         $router = Storage::disk('s3')->put('fotos',$request->image);// Usar el disco 'public' para almacenar las imágenes
 
         $url = Storage::disk('s3')->url($router); // Obtener la URL de la imagen
