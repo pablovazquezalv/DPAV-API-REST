3e4c338461d77f483c67f721e88c44060190a629
yordiortiz09 <114593862+yordiortiz09@users.noreply.github.com>
2024-08-15 14:43:15 -0600

    redirigir a dpav

--- resources/views/contraseña-cambiada.blade.php
+++ resources/views/contraseña-cambiada.blade.php
@@ -17,7 +17,11 @@
         <br>
         <p>¡Tu contraseña ha sido cambiada con éxito!</p>
         <br>
-        <a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar Sesión</a>
+        <a 
+          href="https://dpav.shop/" 
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Iniciar Sesión
+        </a>
     </div>
     
 </body>
