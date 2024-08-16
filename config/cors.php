95e0811d19d8d2264121aa851a4b14c17090792b
Pablo <pabloalvaradovazquez10@gmail.com>
2024-08-15 18:07:38 -0600

    push certificado

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://dpav.shop'
-    , 'http://localhost:42000'
+    
 ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
