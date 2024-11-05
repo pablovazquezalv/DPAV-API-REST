e526828a84dafad407b24d94fe0db519ff3009c5
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-11-04 18:58:30 -0600

    push

--- resources/views/crear-cuenta.blade.php
+++ resources/views/crear-cuenta.blade.php
@@ -5,18 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cuenta creada!</title>
+    <style>
+        .btn-primary {
+            background-color: #007bff;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            text-decoration: none;
+            font-size: 16px;
+            cursor: pointer;
+            display: inline-block;
+            border-radius: 4px;
+        }
+    </style>
 </head>
 <body>
     <h1>Cuenta creada!</h1>
-    <p>Hola {{$user->name}},Tu correo es valido, {{$user->email}}</p>
-    <p>Para activar la cuenta, por favor haga click en el siguiente enlace:</p>
-    <button type="button" class="btn btn-primary">
-        <a href="{{$url}}">
-            
-        Activar cuenta</a></button>
-        <p>{{$url}}</p>
+    <p>Hola {{$user->name}}, Tu correo es válido: {{$user->email}}</p>
+    <p>Para activar la cuenta, por favor haga clic en el siguiente botón:</p>
+    <a href="{{$url}}" class="btn-primary">Activar cuenta</a>
+       
     <p>Si no ha solicitado la creación de la cuenta, por favor ignore este mensaje.</p>
     <p>Gracias!</p>
-    
 </body>
 </html>
