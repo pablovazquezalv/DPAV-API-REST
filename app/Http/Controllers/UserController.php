94bd35e7044a2ae8580d3b49e8046fb972443c3c
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-11-19 18:19:02 -0600

    CAMBIOS API DPAV PRUEBAS Y MAUI

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
             'email' => $request->email,
             
             'password' => Hash::make($request->password),
-            'activo' => 0, // '0' es el estado 'inactivo
+            'activo' => 1, // '0' es el estado 'inactivo
             'role_id' => 3, // '1' es el id del rol 'user
             'numero' => $request->numero,
             'codigo'=> rand(100000, 999999), //genera un código aleatorio de 6 dígitos (opcional
@@ -133,154 +133,111 @@ class UserController extends Controller
         return response()->json('Sesión cerrada', 200);
     }
 
-    public function verificarCodigo(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'codigo' => 'required|string|max:6',
-        ],
-        [
-            'email.required' => 'El email es requerido',
-            'codigo.required' => 'El código es requerido',
-            'email.email' => 'El email no es válido',
-            'codigo.max' => 'El código debe tener 6 caracteres',
+    // public function verificarCodigo(Request $request)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         'email' => 'required|string|email|max:255',
+    //         'codigo' => 'required|string|max:6',
+    //     ],
+    //     [
+    //         'email.required' => 'El email es requerido',
+    //         'codigo.required' => 'El código es requerido',
+    //         'email.email' => 'El email no es válido',
+    //         'codigo.max' => 'El código debe tener 6 caracteres',
             
-        ]);
+    //     ]);
 
-            if ($validator->fails()) {
-                return response()->json($validator->errors(), 400);
-            }
+    //         if ($validator->fails()) {
+    //             return response()->json($validator->errors(), 400);
+    //         }
 
-            $user = User::where('email', $request->email)->first();
+    //         $user = User::where('email', $request->email)->first();
 
             
-            if($user->codigo == $request->codigo)
-            {
-               # $user->codigo = null;
-                $user->activo = 1;
-                $user->save();
+    //         if($user->codigo == $request->codigo)
+    //         {
+    //            # $user->codigo = null;
+    //             $user->activo = 1;
+    //             $user->save();
 
-                return response()->json('Código correcto', 200);
-            }
-            else
-            {
-                return response()->json('Código incorrecto', 400);
-            }
-    }
+    //             return response()->json('Código correcto', 200);
+    //         }
+    //         else
+    //         {
+    //             return response()->json('Código incorrecto', 400);
+    //         }
+    // }
 
-    public function olvideContraseña(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-        ],
-        [
-            'email.required' => 'El email es requerido',
-            'email.email' => 'El email no es válido',
-        ]);
+    // public function olvideContraseña(Request $request)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         'email' => 'required|string|email|max:255',
+    //     ],
+    //     [
+    //         'email.required' => 'El email es requerido',
+    //         'email.email' => 'El email no es válido',
+    //     ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors(), 400);
+    //     }
 
-        $user = User::where('email', $request->email)->first();
+    //     $user = User::where('email', $request->email)->first();
 
-        if($user)
-        {
-            $url = URL::temporarySignedRoute('restablecerContraseña', now()->addMinutes(5), ['id' => $user->id]);
+    //     if($user)
+    //     {
+    //         $url = URL::temporarySignedRoute('restablecerContraseña', now()->addMinutes(5), ['id' => $user->id]);
 
-            Mail::to($user->email)->send(new OlvideContraseña($user, $url));
+    //         Mail::to($user->email)->send(new OlvideContraseña($user, $url));
 
-            return response()->json([
-                'message' => 'Correo enviado',
-                'user' => $user,
-                'url' => $url
-            ]);
-        }
-        else
-        {
-            return response()->json('Usuario no encontrado', 400);
-        }
+    //         return response()->json([
+    //             'message' => 'Correo enviado',
+    //             'user' => $user,
+    //             'url' => $url
+    //         ]);
+    //     }
+    //     else
+    //     {
+    //         return response()->json('Usuario no encontrado', 400);
+    //     }
 
-    }
+    // }
 
-    public function restablecerContraseña(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:8',
-        ],
-        [
-            'email.required' => 'El email es requerido',
-            'email.email' => 'El email no es válido',
-            'password.required' => 'La contraseña es requerida',
-            'password.min' => 'La contraseña debe tener al menos 8 caracteres',
+    // public function restablecerContraseña(Request $request)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         'email' => 'required|string|email|max:255',
+    //         'password' => 'required|string|min:8',
+    //     ],
+    //     [
+    //         'email.required' => 'El email es requerido',
+    //         'email.email' => 'El email no es válido',
+    //         'password.required' => 'La contraseña es requerida',
+    //         'password.min' => 'La contraseña debe tener al menos 8 caracteres',
             
-        ]);
+    //     ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors(), 400);
+    //     }
 
-        $user = User::where('email', $request->email)->first();
+    //     $user = User::where('email', $request->email)->first();
 
-        if($user)
-        {
-            $user->password = Hash::make($request->password);
-            $user->save();
+    //     if($user)
+    //     {
+    //         $user->password = Hash::make($request->password);
+    //         $user->save();
 
-            return response()->json('Contraseña restablecida', 200);
-        }
-        else
-        {
-            return response()->json('Usuario no encontrado', 400);
-        }
+    //         return response()->json('Contraseña restablecida', 200);
+    //     }
+    //     else
+    //     {
+    //         return response()->json('Usuario no encontrado', 400);
+    //     }
 
 
-    }
+    // }
 
-    //RUTAS FIRMADAS - SERVICIOS
-    public function enviarSMS(Request $request)
-    {
-        $user = User::find($request->id);
-
-        if($user->verification_code_sent_at != null)
-        {
-            return response()->json('Código ya enviado', 400);
-        }
-       
-        $response = Http::withHeaders([
-
-            'Authorization' => 'App 16abca2ac5b56ee130ca5c236b16943a-1d3e8a4d-cd81-4bec-96c5-05fb2dbdc14b',
-            'Content-Type' => 'application/json',
-            'Accept' => 'application/json'
-        ])->post('https://2v8jjz.api.infobip.com/sms/2/text/advanced', [
-            'messages' => [
-                [
-                   'destinations' => [
-                       [
-                           'to' => '528718458147'
-                           //'to' => $user->telefono
-                       ]
-                   ],
-                   'from' => 'InfoSMS',
-                   'text' => 'Tu código de verificación es: '.$user->codigo
-                ]
-            ]
-    
-        ]);
-
-        if($response->status() == 200)
-        {
-            $user->verification_code_sent_at = now();
-            $user->email_verified_at = now();
-            $user->save();
-            return response()->view('email/correo-enviado', ['user' => $user]);
-        }
-        else
-        {
-            return response()->json('Error al enviar SMS', 400);
-        }
-    }
-
+   
     
 }
