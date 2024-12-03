8a47e65e3a45c58208997e9378a59c175ec5b89a
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-12-02 19:19:39 -0600

    ws

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Aplicación</title>
-    @vite('resources/js/app.js')  <!-- Asegúrate de que la ruta es correcta -->
 </head>
 <body>
     <div id="app" data-page="{{ json_encode($page) }}"></div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Perros', [
-        'dato' => 'Este es un valor pasado desde Laravel',
-    ]);
+    return view('welcome');
 });
