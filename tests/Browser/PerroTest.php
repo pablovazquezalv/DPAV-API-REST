ed0cf2341968c1e536c17d87ab7708f24fe8a7be
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2024-12-05 00:10:08 -0600

    hola

---
+++ tests/Browser/PerroTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class PerroTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+   
+}
